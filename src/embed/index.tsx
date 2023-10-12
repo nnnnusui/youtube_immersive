80dfab9f85507ec96224e2d733ec6ba361e32102
nnnnusui <nnnnusui@gmail.com>
2023-10-12 14:46:15 +0000

    wait for `ytd-app` rendering

--- src/embed/index.tsx
+++ src/embed/index.tsx
@@ -1,7 +1,9 @@
 import { render } from "solid-js/web";
 
 import { Overlay } from "@/component/Overlay";
+import { querySelectHtmlElementsAsync } from "@/fn/querySelectHtmlElements";
 import { manifest } from "@/manifest";
 
 console.log(`${manifest.name}: loaded.`);
-render(() => <Overlay />, document.querySelector("ytd-app")!);
+querySelectHtmlElementsAsync("ytd-app")
+  .then(([it]) => render(() => <Overlay />, it));
